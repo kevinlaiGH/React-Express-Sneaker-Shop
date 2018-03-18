a1e5322d6596e1fb1aa8d9f0fd88ef1a9e591184
Kevin Lai <kevin@Kevins-MacBook-Pro.local>
2018-03-18 15:00:13 +1100

    fixed error

--- server/main.js
+++ server/main.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = new express();
 
 app.get('/', function (req, res) {
-    res.render('./../app/index.html, {}');
+    res.render('./../app/index.ejs', {});
 })
     .listen(7777);
